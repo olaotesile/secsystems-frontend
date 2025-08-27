d8859db8204feb1017b28d7c6102dec2cb13df07
Olamiposi Otesile <bootesile@gmail.com>
2025-08-27 15:59:31 +0100

    updateeeeeee

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,21 @@
+const BASE_URL =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_BACKEND_URL
+    : import.meta.env.VITE_DEV_BACKEND;
+
 export async function getBanks(query) {
   if (!query) return [];
 
   try {
-    const res = await fetch(`http://localhost:5000/banks?query=${query}`);
+    const res = await fetch(`${BASE_URL}/banks?query=${query}`);
     const json = await res.json();
 
     if (!json.success) return [];
 
-    
     return json.data.map((bank) => ({
-      name: bank.bankName, 
+      name: bank.bankName,
       shortcode: bank.shortcode,
-      logo: bank.logoUrl, 
+      logo: bank.logoUrl,
     }));
   } catch (err) {
     console.error("Error fetching banks:", err);
